44e46cc865e9c163a69867c759feb09761b2a11a
Kushrishi <91156440+Kushrishi@users.noreply.github.com>
2026-05-30 12:48:06 -0700

    Add array basics practice

---
+++ arrays/array_basics.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> numbers = {4, 8, 15, 16, 23, 42};
+
+    int sum = 0;
+    int maxValue = numbers[0];
+
+    for (int num : numbers) {
+        sum += num;
+
+        if (num > maxValue) {
+            maxValue = num;
+        }
+    }
+
+    cout << "Array elements: ";
+    for (int num : numbers) {
+        cout << num << " ";
+    }
+
+    cout << endl;
+    cout << "Sum: " << sum << endl;
+    cout << "Maximum value: " << maxValue << endl;
+
+    return 0;
+}
