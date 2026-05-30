203aadda5a75541d1ddbb2d4065f23a9ca6bea68
Kushrishi <91156440+Kushrishi@users.noreply.github.com>
2026-05-30 13:22:55 -0700

    Add implementation for palindrome check

--- strings/palindrome_check.cpp
+++ strings/palindrome_check.cpp
@@ -1,1 +1,35 @@
+/*
+Topic: Strings
+Concepts: indexing, loops, conditionals
+Purpose: Practice checking whether a string is a palindrome.
+*/
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string word = "racecar";
+    bool isPalindrome = true;
+
+    int left = 0;
+    int right = word.length() - 1;
+
+    while (left < right) {
+        if (word[left] != word[right]) {
+            isPalindrome = false;
+            break;
+        }
+
+        left++;
+        right--;
+    }
+
+    if (isPalindrome) {
+        cout << word << " is a palindrome." << endl;
+    } else {
+        cout << word << " is not a palindrome." << endl;
+    }
+
+    return 0;
+}
